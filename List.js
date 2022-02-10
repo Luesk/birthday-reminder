55707a6e7e624104547bbd442282e08d477c0cf2
Godfred <gabbieluesk1@gmail.com>
2022-02-09 23:05:01 -0500

    first commit

---
+++ List.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const List = ({people}) => {
+  return (
+    <>
+   <h2>list components</h2>
+   {people.map((person)=>{
+     const {id, name, age, image} = person;
+     return (
+       <article key={id} className='person'>
+         <img src={image} alt={name}></img>
+         <div>
+           <h4>{name}</h4>
+           <p>{age} years old</p>
+         </div>
+       </article>
+     )
+   })}
+  </>
+);
+};
+
+export default List;
+
+// const List = ({people}) => {
+//   return (
+//     <>
+//     pe{ople.map((person) =>{
+//       const {id, name, age, image} = person;
+//       return (
+//       <article key={id} className='person'>
+//           <img src={image} alt={name}>{image}</img>
+//           <div>
+//             <h4>{name}</h4>
+//             <p>{age}</p>
+//           </div>
+//       </article>
+//       );
+// })}
+//   </>
+// );
+// };
---
+++ data.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+  {
+    id: 6,
+    name: 'James Henry',
+    age: 25,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGMHCiRRVPdzRDVJAjXqQVoa85OrRS1tCL6w&usqp=CAU',
+  },
+  {
+    id: 7,
+    name: 'Lisa Simpson',
+    age: 23,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRorRnLM6R1JQiABrADyFpYqyo-gHEYyV39ew&usqp=CAU',
+  }
+];
